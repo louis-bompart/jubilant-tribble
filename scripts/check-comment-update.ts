55f3c6c60cb326d7e011316c526fc64c050cc2d5
Louis Bompart <lbompart@coveo.com>
2022-07-19 14:52:29 -0400

    derp

--- scripts/check-comment-update.ts
+++ scripts/check-comment-update.ts
@@ -2,12 +2,14 @@ import { setOutput } from "@actions/core";
 import { context } from "@actions/github";
 import assert from "assert";
 
-assert.equal(context.payload.actor, context.payload.issue.user.login);
-assert.equal(context.payload.comment.user.login, "thetruebaguette[bot]");
+assert.equal(context.actor, context.payload.issue.user.login); // Is the editor the author of the OP
+assert.equal(context.payload.comment.user.login, "thetruebaguette[bot]"); // Is the OG comment writen by the bot
 const before = context.payload.changes.body.from.split("[ ]");
+// Is it just a check of the lil box?
 const after = context.payload.comment.body.split("[x]");
 assert.equal(before[0], after[0]);
 assert.equal(before[1], after[1]);
+// Whats the branch?
 const branchname = /https\:\/\/github.com\/.*\/.*\/tree\/(?<branchname> .*)\s/.exec(context.payload.comment.body).groups.branchname
 setOutput('branchname',branchname);
 
