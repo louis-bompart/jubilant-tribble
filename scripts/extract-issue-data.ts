3d62c77b930f1dc2ca448229645a6d9b9d43f1e1
Louis Bompart <lbompart@coveo.com>
2022-07-19 11:46:39 -0400

    testing

--- scripts/extract-issue-data.ts
+++ scripts/extract-issue-data.ts
@@ -1,8 +1,8 @@
 import { context } from "@actions/github";
 import { setOutput } from "@actions/core";
 
-const issueDataExtractorRegexp = /###\sBranch\/Org name\\n\\n(?<orgname>.*)### Email\\n\\n(?<email>.*)/;
-
+const issueDataExtractorRegexp = /###\sBranch\/Org name\n\n(?<orgname>.*)### Email\n\n(?<email>.*)/gm;
+console.log(context.payload.issue.body)
 const match = context.payload.issue.body.match(issueDataExtractorRegexp);
 
 setOutput('email',match.groups.email);
