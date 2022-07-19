7d5d135ac4902c8fae05bee56e27615f7c0d8a48
Louis Bompart <lbompart@coveo.com>
2022-07-19 11:55:50 -0400

    fix regexp

--- scripts/extract-issue-data.ts
+++ scripts/extract-issue-data.ts
@@ -1,8 +1,8 @@
 import { context } from "@actions/github";
 import { setOutput } from "@actions/core";
 
-const issueDataExtractorRegexp = /###\sBranch\/Org name\s+(?<orgname>.*)### Email\s+(?<email>.*)/gm;
-console.log(Buffer.from(context.payload.issue.body, 'utf-8').toString('base64'))
+const issueDataExtractorRegexp = /###\sBranch\/Org name\s+(?<orgname>.*)\s+### Email\s+(?<email>.*)/gm;
+
 const match = context.payload.issue.body.match(issueDataExtractorRegexp);
 
 setOutput('email',match.groups.email);
